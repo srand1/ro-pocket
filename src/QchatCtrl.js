ed980c766774966d01a5c6056cf619079211cdbf
sudoer <sudoer@penguin>
2022-08-29 08:34:10 +0800

    save & load creds

--- src/QchatCtrl.js
+++ src/QchatCtrl.js
@@ -1,10 +1,26 @@
+import { useRef, useState, Fragment } from 'react';
 import qchatServerList from './qchat.json';
 
 const byName = new Map(qchatServerList.map(({name, qchatServerId}) => [name, qchatServerId]));
 
 export const QchatCtrl = props => {
+	const [accid, setAccid] = useState('');
+	const [pwd, setPwd] = useState('');
+	const saveCreds = () => {
+		window.localStorage.setItem('accid', accid);
+		window.localStorage.setItem('pwd', pwd);
+	};
+	const loadCreds = () => {
+		setAccid(window.localStorage.getItem('accid') || '');
+		setPwd(window.localStorage.getItem('pwd') || '');
+	};
 	return (
 		<>
+			<button onClick={saveCreds}>S</button>
+			<input onChange={evt => setAccid(evt.target.value)} value={accid} />
+			<input onChange={evt => setPwd(evt.target.value)} value={pwd} />
+			<button onClick={loadCreds}>L</button>
+			<br />
 			<input list="qchat-server" onChange={evt => {
 				const v = evt.target.value;
 				const qchatServerId = byName.get(v) || v;
